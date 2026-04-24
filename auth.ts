5c37ae109ea87cba7e4dace149a42ec271c5cc6a
chelsnn <cnnyan@bu.edu>
2026-04-24 18:09:02 -0400

    error with git import

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
-import Github from "@auth/core/providers/github";
+
+import Github from "next-auth/providers/github";
 
 export const {handlers, auth} = NextAuth(
     {
