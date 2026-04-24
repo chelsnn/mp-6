398235d081d1a98c32a83850b75bee9a994bc33e
chelsnn <cnnyan@bu.edu>
2026-04-24 19:08:00 -0400

    changed to fit my version of next auth

--- auth.ts
+++ auth.ts
@@ -1,12 +1,14 @@
-import NextAuth from "next-auth";
+import { NextAuthOptions } from "next-auth";
 
-import Github from "next-auth/providers/github";
+import GithubProvider from "next-auth/providers/github";
 
-export const {handlers, auth} = NextAuth(
-    {
-        providers: [Github({
+export const authOptions: NextAuthOptions = {
+
+    providers: [
+        GithubProvider({
             clientId: process.env.AUTH_GITHUB_ID!,
             clientSecret: process.env.AUTH_GITHUB_SECRET!,
-        })],
-    }
-);
+        }),
+    ],
+};
+
