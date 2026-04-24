60f02b139658a22c605b7ad3bcf7ae519befab51
chelsnn <cnnyan@bu.edu>
2026-04-24 18:15:13 -0400

    error with git again

--- auth.ts
+++ auth.ts
@@ -4,6 +4,9 @@ import Github from "next-auth/providers/github";
 
 export const {handlers, auth} = NextAuth(
     {
-        providers: [Github],
+        providers: [Github({
+            clientId: process.env.AUTH_GITHUB_ID!,
+            clientSecret: process.env.AUTH_GITHUB_SECRET!,
+        })],
     }
 );
